0df58712a76b575880ceb0afd8f34506af0a3d4a
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-07-06 23:40:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,9 @@ sns.set_style("whitegrid")
 
 lottie_tweet = load_lottieurl('https://assets3.lottiefiles.com/packages/lf20_t2xm9bsw.json')
 st_lottie(lottie_tweet, speed=1, height=200, key="initial")
+st.session_state.one_columns_params = (.1, 3.2, .1)
 
-row0sep1, row0col1, row0sep2 = st.columns((.1,3.2,.1))
+row0sep1, row0col1, row0sep2 = st.columns(st.session_state.one_columns_params)
 with row0sep1:
     st.markdown("")
 
@@ -41,82 +42,87 @@ with row0col1:
     st.markdown("""Many researchers find that political polarization has increased in the US over the last two decades. 
                 In particular, studies consistently find that **affective polarization**, i.e., favorable feelings towards own party (the ingroup) and dislike towards the other party (the outgroup), has grown. 
                 This website explores how those who identify with the Republican or Democratic parties describe and feel about the parties.
-                """)
+                """)    
 
+    placeholder = st.empty()
+    with placeholder.container():
+        with st.expander("Consent", expanded=True):
+            st.markdown("""
+            By submitting the form below you agree to your data being used for research. 
+            Your twitter handle will be stored in a private google sheet and will not be shared with anyone (unless extraordinary circumstances force us to share it). 
+            You can ask for your data to be deleted by emailing us with an ID number you'll be issued after submitting the form. 
+            """)
+            agree = st.checkbox("I understand and consent.")
+
+    if agree:
+        placeholder.empty()
+        with st.expander("Consent", expanded=False):
+            st.markdown("""
+            By submitting the form below you agree to your data being used for research. 
+            Your twitter handle will be stored in a private google sheet and will not be shared with anyone (unless extraordinary circumstances force us to share it). 
+            You can ask for your data to be deleted by emailing us with an app ID number you'll be issued after submitting the form. 
+            """)
+            st.markdown("You have consented.")
+     
 with row0sep2:
-    st.markdown("")        
-
-placeholder = st.empty()
-with placeholder.container():
-    with st.expander("Consent", expanded=True):
-        st.markdown("""
-           By submitting the form below you agree to your data being used for research. 
-           Your twitter handle will be stored in a private google sheet and will not be shared with anyone (unless extraordinary circumstances force us to share it). 
-           You can ask for your data to be deleted by emailing us with an ID number you'll be issued after submitting the form. 
-           """)
-        agree = st.checkbox("I understand and consent.")
-
-if agree:
-    placeholder.empty()
-    with st.expander("Consent", expanded=False):
-        st.markdown("""
-           By submitting the form below you agree to your data being used for research. 
-           Your twitter handle will be stored in a private google sheet and will not be shared with anyone (unless extraordinary circumstances force us to share it). 
-           You can ask for your data to be deleted by emailing us with an app ID number you'll be issued after submitting the form. 
-           """)
-        st.markdown("You have consented.")
-    
+    st.markdown("")   
 
 st.session_state.submitted = False
 st.session_state.disable = True 
+st.session_state.two_columns_params = (.1, 1.5, .2, 1.5, .1)
 
 if agree:
-    form_place = st.empty()
-    with form_place.container():
-        form = st.expander("Form",expanded=True)
-        form.text_input("Enter a twitter handle", key="name", placeholder="e.g. @POTUS", value="")
-        st.session_state.username_mine = form.radio(
-            "I confirm that",
-            ('This handle belongs to me.', 'This handle belongs to someone else.')) 
+    formsep1, formcol1, formsep2 = st.columns(st.session_state.one_columns_params)
+    with formsep1:
+        st.markdown("")
 
-        dem_words, rep_words = [], []
-        form.markdown("#### Please add five words that describe Republicans best")
-        for i in range(5):
-            rep_words.append(form.text_input("R"+str(i+1),key = "R"+str(i+1)))
-        st.session_state.rep_words = ", ".join(rep_words).lower()
-        form.markdown("#### Please add five words that describe Democrats best")
-        for i in range(5):
-            dem_words.append(form.text_input("D"+str(i+1), key = "D"+str(i+1)))
-        st.session_state.dem_words = ", ".join(dem_words).lower()
+    with formcol1:
+        form_place = st.empty()
+        with form_place.container():
+            form = st.expander("Form",expanded=True)
+            form.text_input("Enter a twitter handle", key="name", placeholder="e.g. @POTUS", value="")
+            st.session_state.username_mine = form.radio(
+                "I confirm that",
+                ('This handle belongs to me.', 'This handle belongs to someone else.')) 
 
-        form.markdown("#### Feeling Thermometer")
-        form.markdown("""The **feeling thermometer** is a popular tool in psychology and political science that is meant to measure how a person feels towards a particular group or individual. 
+            dem_words, rep_words = [], []
+            form.markdown("#### Please add five words that describe Republicans best")
+            for i in range(5):
+                rep_words.append(form.text_input("R"+str(i+1),key = "R"+str(i+1)))
+            st.session_state.rep_words = ", ".join(rep_words).lower()
+            form.markdown("#### Please add five words that describe Democrats best")
+            for i in range(5):
+                dem_words.append(form.text_input("D"+str(i+1), key = "D"+str(i+1)))
+            st.session_state.dem_words = ", ".join(dem_words).lower()
+
+            form.markdown("#### Feeling Thermometer")
+            form.markdown("""The **feeling thermometer** is a popular tool in psychology and political science that is meant to measure how a person feels towards a particular group or individual. 
                         Please rate each party using the feeling thermometer. 
                         You can give a rating between 0 (the coldest) and 100 (the warmest).
                         Ratings **above 50** degrees mean you feel positive or warm toward the group. 
                         Ratings **below 50** degrees mean you feel negative or cold toward the group. 
                         If you don't feel anything toward the group, give a rating of 50.""")
 
-        form.slider("How do you feel about Republicans?", 
+            form.slider("How do you feel about Republicans?", 
                     min_value=0, max_value=100, value=50, step=1,key="rep_temp")          
-        form.slider("How do you feel about Democrats?", 
+            form.slider("How do you feel about Democrats?", 
                         min_value=0, max_value=100, value=50, step=1,key="dem_temp") 
-        st.session_state.party = form.radio(
+            st.session_state.party = form.radio(
                      "How do you identify?",
                     ('Independent','Republican', 'Democrat')) 
-        st.session_state.disable = True if st.session_state.D5 == "" else False
+            st.session_state.disable = True if st.session_state.D5 == "" else False
  
-        form.warning("Please fill out every field of the form to enable the submit button.")              
-        st.session_state.submitted = form.button("Submit", disabled=st.session_state.disable)
-    if  st.session_state.submitted:
-        form_place.empty()
+            form.warning("Please fill out every field of the form to enable the submit button.")              
+            st.session_state.submitted = form.button("Submit", disabled=st.session_state.disable)
+        if  st.session_state.submitted:
+            form_place.empty()
 
-    with st.expander("Form Submitted",expanded=True):
-        if st.session_state.submitted:
-            st.session_state.id = datetime.now().strftime('%Y%m-%d%H-%M-') + str(uuid4())
-            st.markdown(f"Thanks for submitting your answers! Your app ID is {st.session_state.id}. Email us with it if you want your answers deleted.") 
+        with st.expander("Form Submitted",expanded=True):
+            if st.session_state.submitted:
+                st.session_state.id = datetime.now().strftime('%Y%m-%d%H-%M-') + str(uuid4())
+                st.markdown(f"Thanks for submitting your answers! Your app ID is {st.session_state.id}. Email us with it if you want your answers deleted.") 
                         
-            st.session_state.conn = connect(":memory:", 
+                st.session_state.conn = connect(":memory:", 
                             adapter_kwargs = {
                             "gsheetsapi": { 
                             "service_account_info":  st.secrets["gcp_service_account"] 
@@ -124,34 +130,37 @@ if agree:
                                         }
                         )
 
-            #insert_user_data(st.session_state.conn, st.secrets["private_gsheets_url"])
+                #insert_user_data(st.session_state.conn, st.secrets["private_gsheets_url"])
 
-    if st.session_state.submitted and 'df' not in st.session_state:
-        with st.spinner(text="Retrieving data..."):
-            sheet_url = st.secrets["private_gsheets_url"]
-            query = f'SELECT * FROM "{sheet_url}"'
-            st.session_state.df = make_dataframe(st.session_state.conn.execute(query))
+        if st.session_state.submitted and 'df' not in st.session_state:
+            with st.spinner(text="Retrieving data..."):
+                sheet_url = st.secrets["private_gsheets_url"]
+                query = f'SELECT * FROM "{sheet_url}"'
+                st.session_state.df = make_dataframe(st.session_state.conn.execute(query))
 
-    if st.session_state.submitted and 'df' in st.session_state:    
+        if st.session_state.submitted and 'df' in st.session_state:    
 
-        with st.spinner(text="Making graphs..."):
-            all_dem_words = list(st.session_state.df.query("party=='Republican'").dem_words)
-            all_rep_words = list(st.session_state.df.query("party=='Democrat'").rep_words)
-            outgroup_cloud, most_common_out = make_v_wordcloud(all_rep_words,all_dem_words, ['Democrats about Republicans', 'Republicans about Democrats'])   
+            with st.spinner(text="Making graphs..."):
+                all_dem_words = list(st.session_state.df.query("party=='Republican'").dem_words)
+                all_rep_words = list(st.session_state.df.query("party=='Democrat'").rep_words)
+                outgroup_cloud, most_common_out = make_v_wordcloud(all_rep_words,all_dem_words, ['Democrats\nView\nRepublicans', 'Republicans\nView\nDemocrats'])   
 
-            all_dem_words = list(st.session_state.df.query("party=='Democrat'").dem_words)
-            all_rep_words = list(st.session_state.df.query("party=='Republican'").rep_words)
-            ingroup_cloud, most_common_in = make_v_wordcloud(all_rep_words,all_dem_words,['Republicans about Republicans','Democrats about Democrats']) 
+                all_dem_words = list(st.session_state.df.query("party=='Democrat'").dem_words)
+                all_rep_words = list(st.session_state.df.query("party=='Republican'").rep_words)
+                ingroup_cloud, most_common_in = make_v_wordcloud(all_rep_words,all_dem_words,['Republicans\nView\nRepublicans','Democrats\nView\nDemocrats']) 
 
-            group_means = st.session_state.df.groupby("party").agg('mean') 
-            outgroup = pd.DataFrame({'party':['Republicans\n View\n Democrats', 'Democrats\n View\n Republicans'], 
+                group_means = st.session_state.df.groupby("party").agg('mean') 
+                outgroup = pd.DataFrame({'party':['Republicans\nView\nDemocrats', 'Democrats\nView\nRepublicans'], 
                                 'temp': [group_means.loc['Republican','dem_temp'],group_means.loc['Democrat','rep_temp']] })
-            ingroup = pd.DataFrame({'party':['Republicans\n View\n Republicans', 'Democrats\n View\n Democrats'], 
+                ingroup = pd.DataFrame({'party':['Republicans\nView\nRepublicans', 'Democrats\nView\nDemocrats'], 
                                 'temp': [group_means.loc['Republican','rep_temp'], group_means.loc['Democrat','dem_temp']] })
         
+        with formsep2:
+            st.markdown("")
+
         st.markdown("")
     
-        row1sep1, row1col1, row1sep2, row1col2, row1sep3 = st.columns((.1, 1.5, .2, 1.5, .1))  
+        row1sep1, row1col1, row1sep2, row1col2, row1sep3 = st.columns(st.session_state.two_columns_params)
         with row1sep1:
             st.markdown("")
 
@@ -173,7 +182,7 @@ if agree:
 
         st.markdown("")
 
-        row2sep1, row2col1, row2sep2, row2col2, row2sep3 = st.columns((.1, 1.5, .2, 1.5, .1))  
+        row2sep1, row2col1, row2sep2, row2col2, row2sep3 = st.columns(st.session_state.two_columns_params) 
         with row2sep1:
             st.markdown("")
         with row2col1:
@@ -185,7 +194,7 @@ if agree:
             axiz1.set_ylabel('Feeling Thermometer Score')
             axiz1.set_yticks(range(0,100,5))
             axiz1.set_xlabel('')
-            axiz1.set_xticklabels(labels=ingroup["party"],rotation=45)
+            axiz1.set_xticklabels(labels=ingroup["party"])
             axiz1.set(ylim=(0, 100))
             st.pyplot(fig1) 
 
@@ -201,7 +210,7 @@ if agree:
             axiz2.set_ylabel('Feeling Thermometer Score')
             axiz2.set_xlabel('')
             axiz2.set_yticks(range(0,100,5))
-            axiz2.set_xticklabels(labels=outgroup["party"],rotation=45)
+            axiz2.set_xticklabels(labels=outgroup["party"])
             axiz2.set(ylim=(0, 100))
             st.pyplot(fig2)
 
@@ -213,7 +222,6 @@ if agree:
         st.markdown("***")
         st.markdown("""Thank you for going through this analysis. 
                         If you have any comments, ideas or feedback, please reach out to me on Twitter [@YaraKyrychenko](https://twitter.com/YaraKyrychenko).""")
-        st.markdown("Tweet about this app:")
         components.html(
             """
             <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" 
